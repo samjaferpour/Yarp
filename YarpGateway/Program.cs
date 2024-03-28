fc11e70aeed9ada94b12facaf6c0f9439b74a968
300092428 <300092428@omidtech-co.local>
2024-03-28 15:40:02 +0330

    Add Rate Limiter

--- YarpGateway/Program.cs
+++ YarpGateway/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,8 +9,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add Yarp
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("Yarp"));
+
+// Add Yarp Rate Limiting
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(5);
+        options.PermitLimit = 2;
+    });
+});
+
 
 var app = builder.Build();
 
@@ -24,5 +38,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.MapReverseProxy();
+
+app.UseRateLimiter();
 
 app.Run();
