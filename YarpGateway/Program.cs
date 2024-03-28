78d602b1d4767bbc03858ce84dbcedaac03ea5fa
300092428 <300092428@omidtech-co.local>
2024-03-28 16:45:15 +0330

    Add Jwt Auth and test and comment it

--- YarpGateway/Program.cs
+++ YarpGateway/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +28,31 @@ builder.Services.AddRateLimiter(rateLimiterOptions =>
 });
 
 
+/*// Add JWT authentication services
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true, // Set to true if you want to validate the JWT issuer
+            ValidateAudience = true, // Set to true if you want to validate the JWT audience
+            ValidateLifetime = true, // Set to true if you want to validate the token expiration
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "yourIssuer", // Replace with the issuer of your JWT tokens
+            ValidAudience = "yourAudience", // Replace with the audience of your JWT tokens
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("yourSecretKey")) // Replace with your JWT secret key
+        };
+    });
+
+
+// Add Authorization to Yarp
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("secure", policy =>
+    policy.RequireAuthenticatedUser());
+});*/
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -33,11 +62,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
-app.MapControllers();
-
 app.MapReverseProxy();
+
+app.MapControllers();
 
 app.UseRateLimiter();
 
